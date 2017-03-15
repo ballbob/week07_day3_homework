1a86f864d275b0948ebf0ad06ee8e7d19b0a0508
Robert Ball <robert.ball.1989@gmail.com>
2017-03-15 16:32:10 +0000

    Class SquidLister created.

--- app/src/main/java/com/codeclan/listsquids/SquidLister.java
+++ app/src/main/java/com/codeclan/listsquids/SquidLister.java
@@ -10,4 +10,18 @@ import java.util.ArrayList;
 public class SquidLister {
 
     private ArrayList<Squid> list;
+
+    public SquidLister(){
+        list = new ArrayList<Squid>();
+        list.add(new Squid(750, "Colossal squid", "Mesonychoteuthis hamiltoni"));
+        list.add(new Squid(275, "Giant squid", "Architeuthis dux"));
+        list.add(new Squid(274, "Humboldt squid", "Dosidicus gigas"));
+        list.add(new Squid(3, "Pacific bigfin squid", "Magnapinna pacifica"));
+        list.add(new Squid(2, "Strawberry squid", "Histiotheuthis heteropsis"));
+        list.add(new Squid(1, "European squid", "Loligo vulgaris"));
+    }
+
+    public ArrayList<Squid> getList(){
+        return new ArrayList<Squid>(list);
+    }
 }
